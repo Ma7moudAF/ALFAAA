b3ce8d1d10221aaa4e1f6a920ad73794210272c7
MADISON111110 <88980411+MADISON111110@users.noreply.github.com>
2021-09-12 01:33:17 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -21452,7 +21452,7 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5  
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/EX_2005&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
-if text == 'شيكو' or text == 'المبرمج شيكو then  
+if text == 'شيكو' or text == 'المبرمج شيكو' then  
 local Text = [[  
 للتواصل مع المبرمج شيكو 📊📮.
 اتبع الازرار الاتيه 📊. 
